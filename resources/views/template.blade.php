ac7f588e38bed104b5c8dc7ec1983098672cc77a
florence.labrador@republisys.com <florence.labrador@republisys.com>
2017-02-01 16:08:06 +0800

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,52 +1,19 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link href="{{ URL('css/template.css') }}" rel="stylesheet" type="text/css">
 
-<nav class="flo-nav">
-    <div class="flo-menu-container">
-        <ul class="flo-primary-menu green">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Product</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-</nav>
-
-
-<nav class="flo-nav">
-    <div class="flo-menu-container">
-        <ul class="flo-primary-menu orange">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Product</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-</nav>
-
-
-<nav class="flo-nav">
-    <div class="flo-menu-container">
-        <ul class="flo-primary-menu yellow">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Product</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-</nav>
-
-
-<nav class="flo-nav">
-    <div class="flo-menu-container">
-        <ul class="flo-primary-menu pink">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Product</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-</nav>
+<div class="wrapper">
+<header id="masthead">
+    <nav class="flo-nav">
+        <div class="flo-menu-container">
+            <ul class="flo-primary-menu green">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
 
 <button class="flo-btn green">Submit</button>
 <button class="flo-btn orange">Submit</button>
@@ -54,16 +21,17 @@
 <button class="flo-btn pink">Submit</button>
 
 
-
-<div class="flo-container">
-    <section class="flo-section">
-        <h2 class="label-yellow">Neque porro quisquam est qui dolorem ipsum quia dolor sit...</h2>
-        <h2 class="label-white">Neque porro quisquam est qui dolorem ipsum quia dolor sit...</h2>
-    </section>
+<div style="background: url('https://awal.com/wp-content/uploads/2016/03/thom_yorke.jpg') center center / cover no-repeat fixed">
+    <div class="flo-container">
+        <section class="flo-section">
+            <h2 class="label-yellow">Neque porro quisquam est qui dolorem ipsum quia dolor sit...</h2>
+            <h2 class="label-white">Neque porro quisquam est qui dolorem ipsum quia dolor sit...</h2>
+        </section>
+    </div>
 </div>
 
 <div class="flo-container">
-    <section class="flo-section">
+    <section class="flo-section three-columns">
         <div class="flo-column">
             <img src="">
             <h4 class="label-yellow">Lorem Ipsum</h4>
@@ -80,4 +48,42 @@
             <p>is simply dummy text of the printing and typesetting industry.</p>
         </div>
     </section>
+</div>
+
+<div style="background: url('https://awal.com/wp-content/uploads/2017/01/Yung_Lean.jpg') center center / cover no-repeat fixed">
+    <div class="flo-container">
+        <section class="flo-section one-column">
+            <div class="flo-column">
+                <h1 class="label-orange">Dolorem Ipsum Quia Dlor</h1>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+        </section>
+    </div>
+</div>
+
+<div class="flo-container">
+    <section class="flo-section two-columns">
+        <h1 class="label-green">Contact Us</h1>
+
+        <div class="flo-column">
+            <h3 class="label-white">Dolorem Ipsum Quia</h3>
+            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        </div>
+
+        <div class="flo-column">
+            <form class="signup-box" action="" method="post">
+                <input type="text" class="flo-input" placeholder="First Name">
+                <input type="text" class="flo-input" placeholder="Last Name">
+                <input type="email" class="flo-input" placeholder="Email">
+
+                <input class="flo-btn green" type="submit" value="Apply">
+            </form>
+        </div>
+    </section>
+</div>
+
+
+<div class="title-flowt">FLORENCE</div>
+
+<div class="modal-backdrop fade in"></div>
 </div>
