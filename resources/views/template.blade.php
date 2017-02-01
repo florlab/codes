9c3ef7f99e8040bdaf4aef96eec8d49c0a9708c6
florence.labrador@republisys.com <florence.labrador@republisys.com>
2017-02-01 13:21:41 +0800

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -53,3 +53,31 @@
 <button class="flo-btn yellow">Submit</button>
 <button class="flo-btn pink">Submit</button>
 
+
+
+<div class="flo-container">
+    <section class="flo-section">
+        <h2 class="label-yellow">Neque porro quisquam est qui dolorem ipsum quia dolor sit...</h2>
+        <h2 class="label-white">Neque porro quisquam est qui dolorem ipsum quia dolor sit...</h2>
+    </section>
+</div>
+
+<div class="flo-container">
+    <section class="flo-section">
+        <div class="flo-column">
+            <img src="">
+            <h4 class="label-yellow">Lorem Ipsum</h4>
+            <p>is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="flo-column">
+            <img src="">
+            <h4 class="label-yellow">Lorem Ipsum</h4>
+            <p>is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div class="flo-column">
+            <img src="">
+            <h4 class="label-yellow">Lorem Ipsum</h4>
+            <p>is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+    </section>
+</div>
